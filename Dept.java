c4fdb4a916f952eafd79b08f4a9f54c4d1ce74e7
huanzx6789 <51231063+huanzx6789@users.noreply.github.com>
2019-05-31 21:11:33 +0800

    Update Dept.java

--- Dept.java
+++ Dept.java
@@ -3,5 +3,6 @@
 public class Dept implements Serializable {
 	private Integer deptno ;
 	private String address ;
-	---------------------------------
+	//------------test---------------------
+	System.out.println("OK ?") ;
 }
